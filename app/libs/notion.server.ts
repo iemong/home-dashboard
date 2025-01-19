ad3f42009944fbfc5ec955956de53fd9d6ba41db
iemong <kiyoshidainagon@gmail.com>
2025-01-19 17:37:06 +0900

    Remove timezone handling in getTodayChildCareLogs function for simplified date retrieval

--- app/libs/notion.server.ts
+++ app/libs/notion.server.ts
@@ -65,7 +65,7 @@ export const getChildCareTotalsByDate = async () => {
 }
 
 export const getTodayChildCareLogs = async () => {
-  const now = toZonedTime(new Date(), 'Asia/Tokyo');
+  const now = new Date();
   const todayStart = startOfDay(now);
   
   const response = await client.databases.query({
